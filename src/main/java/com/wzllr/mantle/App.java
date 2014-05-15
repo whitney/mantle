023f2e0b1f293052f23a2073cab0bc8fee0994c4
Whitney Zoller <whitneyrzoller@gmail.com>
2014-05-14 23:10:11 -0400

    Engine stubs

--- src/main/java/com/wzllr/mantle/App.java
+++ src/main/java/com/wzllr/mantle/App.java
@@ -1,13 +1,44 @@
 package com.wzllr.mantle;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.wzllr.mantle.engine.Window;
+
+public class App {
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final String TITLE = "Mantle";
+
+    public App() {
+
+    }
+
+    public void start() {
+        run();
+    }
+
+    public void stop() {
+
+    }
+
+    public void run() {
+        while (!Window.isCloseRequested()) {
+            render();
+        }
+    }
+
+    public void render() {
+        Window.render();
+    }
+
+    public void cleanUp() {
+
+    }
+
+    public static void main( String[] args ) {
+        Window.createWindow(WIDTH, HEIGHT, TITLE);
+
+        App game = new App();
+
+        game.start();
     }
 }
