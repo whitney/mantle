7c81e547aff9738da12fb5afe20e09a6e5b7e11d
Whitney Zoller <whitneyrzoller@gmail.com>
2014-05-16 00:41:05 -0400

    basic main loopwq

--- src/main/java/com/wzllr/mantle/App.java
+++ src/main/java/com/wzllr/mantle/App.java
@@ -1,5 +1,7 @@
 package com.wzllr.mantle;
 
+import com.wzllr.mantle.engine.Game;
+import com.wzllr.mantle.engine.Time;
 import com.wzllr.mantle.engine.Window;
 
 public class App {
@@ -7,31 +9,95 @@ public class App {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
     public static final String TITLE = "Mantle";
+    public static final double FRAME_CAP = 5000.0;
+
+    private boolean isRunning;
+    private Game game;
 
     public App() {
-
+        isRunning = false;
+        game = new Game();
     }
 
     public void start() {
+        if (isRunning)
+            return;
+
         run();
     }
 
     public void stop() {
+        if (!isRunning)
+            return;
 
+        isRunning = false;
     }
 
-    public void run() {
-        while (!Window.isCloseRequested()) {
-            render();
+    private void run() {
+        isRunning = true;
+
+        // FPS count
+        int frames = 0;
+        long frameCounter = 0;
+
+        final double frameTime = 1.0 / FRAME_CAP;
+
+        long lastTime = Time.getTime();
+        double unprocessedTime = 0;
+
+        while (isRunning) {
+            boolean render = false;
+
+            long startTime = Time.getTime();
+            long elapsedTime = startTime - lastTime;
+            lastTime = startTime;
+
+            unprocessedTime += elapsedTime / (double)Time.SECOND;
+            frameCounter += elapsedTime;
+
+            while (unprocessedTime > frameTime) {
+                unprocessedTime -= frameTime;
+
+                render = true;
+
+                if (Window.isCloseRequested()) {
+                    stop();
+                }
+
+                Time.setDelta(frameTime);
+
+                game.input();
+                game.update();
+
+                if (frameCounter >= Time.SECOND) {
+                    System.out.println(frames);
+                    frames = 0;
+                    frameCounter = 0;
+                }
+            }
+
+            if (render) {
+                render();
+                frames++;
+            } else {
+                try {
+                Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+        cleanUp();
     }
 
-    public void render() {
+    private void render() {
+        game.render();
         Window.render();
     }
 
-    public void cleanUp() {
-
+    private void cleanUp() {
+        Window.dispose();
     }
 
     public static void main( String[] args ) {
--- src/main/java/com/wzllr/mantle/engine/Window.java
+++ src/main/java/com/wzllr/mantle/engine/Window.java
@@ -4,13 +4,6 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
-/**
- * Created with IntelliJ IDEA.
- * User: whitney
- * Date: 5/13/14
- * Time: 11:02 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Window {
     public static void createWindow(int width, int height, String title) {
         Display.setTitle(title);
@@ -20,6 +13,10 @@ public class Window {
         } catch (LWJGLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void dispose() {
+        Display.destroy();
     }
 
     public static void render() {
